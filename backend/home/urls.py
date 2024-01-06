1a9dd1cb9630695a70c477bfb928fa3fe5b23f99
LightningMonster <lightningmonster.1548@gmail.com>
2024-01-06 18:19:51 +0530

    sign up page added

--- backend/home/urls.py
+++ backend/home/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
 from home import views
+from .views import login_view
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', login_view, name='login'),
     path('contact/', views.contact, name='home'),
 ]
--- backend/home/views.py
+++ backend/home/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            # Redirect to the home page or any other desired page upon successful login
+            return redirect('home')
+
+    return render(request, 'login.html')
 
 def home(request):
     return render(request, 'index.html')
@@ -8,5 +24,22 @@ def login(request):
 
 def contact(request):
     return render(request, 'contactpage.html')
-    
-# Create your views here.
+
+def signup(request):
+        if request.method == "POST":
+            firstName = request.POST['firstName']
+            lastName = request.POST['lastName']
+            username = request.POST['username']
+            email = request.POST['email']
+            password = request.POST['password']
+            confirmPassword = request.POST['confirmPassword']
+        
+            myuser = User.objects.create_user(username, email, password)
+            myuser.first_name = firstName
+            myuser.last_name = lastName
+            myuser.save()
+            messages.success(request, "Account created successfully!")
+            return render(request, 'index.html')
+
+        return render(request, 'signup.html')
+        
