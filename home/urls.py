8368ec81e37fe59ee9d81a08c87162161f86e8ed
LightningMonster <lightningmonster.1548@gmail.com>
2024-01-10 19:46:36 +0530

    removing login.html and adding Login_Page.html

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('contact/', views.contact, name='contact'),
 ]
