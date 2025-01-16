cc76539958a25b0b74c2030498a0e4d7e997f664
Omkar04k <omkar.yt04@gmail.com>
2025-01-16 21:10:51 +0530

    test

--- Stock_Site/myenv/bin/activate.fish
+++ Stock_Site/myenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/yash/Desktop/Stock_Project/Web_Project/Stock_Site/myenv
+set -gx VIRTUAL_ENV /home/omkar/Desktop/project/Web_Project/Stock_Site/myenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
